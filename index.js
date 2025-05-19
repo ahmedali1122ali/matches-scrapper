e504fa3b3b123e468232783f2a1d55f79bd2e476
Siddiq Ahmed <siddiqahmed882@gmail.com>
2025-05-20 00:27:04 +0500

    ...

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
 import express from 'express';
 import scrapper from './scrapping.js';
+import cors from 'cors';
 
 const app = express();
 const PORT = 3000;
 
-app.get('/api', (req, res) => {
-    scrapper()
-        .then((matches) => {
-            res.json(matches);
-        })
-        .catch((error) => {
-            console.error('Error:', error);
-            res.status(500).json({ error: 'An error occurred while scraping data.' });
-        });
+app.use(cors({ origin: '*' }));
+
+app.get('/api/matches', (req, res) => {
+  scrapper()
+    .then((matches) => {
+      res.json(matches);
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+      res.status(500).json({ error: 'An error occurred while scraping data.' });
+    });
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
