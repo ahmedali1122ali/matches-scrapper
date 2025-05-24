2c68357372741a35290c8a3787c58209d323e46a
ahmedali1122ali <yaraenas3@gmail.com>
2025-05-25 01:26:21 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,54 +2,26 @@ import express from 'express';
 import { scrapper } from './scrapping.js';
 import cors from 'cors';
 import moment from 'moment-timezone';
+import cron from 'node-cron';
+import { prismaInstance } from './db/prisma-instance.js';
 
 const app = express();
 const PORT = 3000;
-
-const CACHE_DURATION_IN_MILLI_SECONDS = 1000 * 60 * 60; // 1hour
+console.log(PORT);
 
 const TIMEZONE = 'Africa/Cairo';
-
-// Cache object
-let cache = {
-  data: null,
-  fetchedAt: 0,
-  cairoMidnight: 0,
-};
-
-// Helper to get next Cairo midnight timestamp
-function getNextCairoMidnight() {
-  const now = moment().tz(TIMEZONE);
-  return now.clone().add(1, 'day').startOf('day').valueOf();
-}
 
 app.use(cors({ origin: '*' }));
 
 app.get('/api/matches', async (req, res) => {
-  const now = moment().tz(TIMEZONE).valueOf();
-
-  const dontServeCache = req.query['no-cache'] === 'true';
-
-  // Check if cache is valid: not expired and not past Cairo midnight
-  const isCacheValid =
-    !dontServeCache &&
-    cache.data &&
-    now - cache.fetchedAt < CACHE_DURATION_IN_MILLI_SECONDS &&
-    now < cache.cairoMidnight;
-
-  if (isCacheValid) {
-    return res.json(cache.data);
-  }
-
   try {
-    const { isErrored, results } = await scrapper();
-    if (!isErrored) {
-      cache = {
-        data: results,
-        fetchedAt: now,
-        cairoMidnight: getNextCairoMidnight(),
-      };
-    }
+    const todayPromise = prismaInstance.today.findMany();
+    const tomorrowPromise = prismaInstance.tomorrow.findMany();
+    const yesterdayPromise = prismaInstance.yesterday.findMany();
+
+    const [today, tomorrow, yesterday] = await Promise.all([todayPromise, tomorrowPromise, yesterdayPromise]);
+
+    const results = { today, tomorrow, yesterday };
     res.json(results);
   } catch (error) {
     console.error('Error:', error);
@@ -57,6 +29,45 @@ app.get('/api/matches', async (req, res) => {
   }
 });
 
+async function scrapeAndSave() {
+  try {
+    const { results } = await scrapper();
+
+    console.log('Cron job running...', results);
+
+    if (results.today) {
+      await prismaInstance.today.deleteMany();
+
+      await prismaInstance.today.createMany({
+        data: results.today,
+      });
+    }
+
+    if (results.tomorrow) {
+      await prismaInstance.tomorrow.deleteMany();
+
+      await prismaInstance.tomorrow.createMany({
+        data: results.tomorrow,
+      });
+    }
+
+    if (results.yesterday) {
+      await prismaInstance.yesterday.deleteMany();
+
+      await prismaInstance.yesterday.createMany({
+        data: results.yesterday,
+      });
+    }
+  } catch (error) {
+    console.log('Error in cron job:', error);
+  }
+}
+
+cron.schedule('0 0 * * *', scrapeAndSave, {
+  timezone: TIMEZONE,
+});
+
 app.listen(PORT, () => {
+  scrapeAndSave();
   console.log(`Server is running on http://localhost:${PORT}`);
 });
